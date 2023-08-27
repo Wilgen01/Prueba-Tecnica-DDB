a12dcf1dc5e1ac5c695d223673ec0122d45ff74a
Wilgen Garcia <wilgengarcia2001@gmail.com>
2023-08-26 19:46:00 -0500

    solucionando test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        NavbarComponent,
+        FooterComponent
       ],
       declarations: [
         AppComponent
@@ -26,10 +30,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('Prueba-Tecnica-DDB');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Prueba-Tecnica-DDB app is running!');
-  });
 });
--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { PipesModule } from 'src/app/shared/pipes/pipes.module';
+import { HelpComponent } from 'src/app/shared/help/help.component';
+import { FilterModalComponent } from 'src/app/components/filter-modal/filter-modal.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,6 +12,12 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        PipesModule,
+        HelpComponent,
+        FilterModalComponent
+      ],
       declarations: [ HomeComponent ]
     })
     .compileComponents();
